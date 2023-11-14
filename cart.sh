738f9a28162a561b0238e284a6aa8174cb624ee0
manoj <gandhamanojkutty@gmail.com>
2023-11-14 13:56:38 +0530

    code updated

--- cart.sh
+++ cart.sh
@@ -1,31 +1,31 @@
-echo "----------configure nodejs repos---------"
+echo -e "\e[32m--------configure nodejs repos-------\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo "----------install nodejs---------"
+echo -e "\e[32m--------install nodejs-------\e[0m"
 dnf install nodejs -y
 
-echo "----------add application user---------"
+echo -e "\e[32m--------add application user-------\e[0m"
 useradd roboshop
 
-echo "----------create application dir---------"
+echo -e "\e[32m--------create application dir-------\e[0m"
 rm -rf /app
 mkdir /app
 
-echo "----------download app content---------"
+echo -e "\e[32m--------download app content-------\e[0m"
 curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 
-echo "----------unzip app content---------"
+echo -e "\e[32m--------unzip app content-------\e[0m"
 unzip /tmp/cart.zip
 
-echo "----------install dependencies---------"
+echo -e "\e[32m--------install npm dependencies-------\e[0m"
 npm install
 
-echo "----------copy catalogue systemd service file ---------"
+echo -e "\e[32m--------copy cart systemd service file-------\e[0m"
 cd /home/centos/roboshop-shell
 cp cart.service /etc/systemd/system/cart.service
 
-echo "----------start catalogue service---------"
+echo -e "\e[32m--------start cart service-------\e[0m"
 systemctl daemon-reload
 systemctl enable cart
 systemctl restart cart
