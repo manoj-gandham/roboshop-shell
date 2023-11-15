3fd79ef06babc82b0a614fc43b354d423ba722ae
manoj <gandhamanojkutty@gmail.com>
2023-11-15 13:04:54 +0530

    code updated

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
