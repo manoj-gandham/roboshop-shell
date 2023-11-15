7ab6089cd5482ccb40d7a263268062310d06ac01
manoj <gandhamanojkutty@gmail.com>
2023-11-15 16:06:59 +0530

    code updated

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -10,11 +10,11 @@ echo -e "\e[36m>>>>>>>>> Setup RabbitMQ Repos <<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
 
 echo -e "\e[36m>>>>>>>>> Install ErLang & RabbitMQ <<<<<<<<\e[0m"
-yum install erlang rabbitmq-server -y
+dnf install rabbitmq-server -y
 
 echo -e "\e[36m>>>>>>>>> Start RabbitMQ Service <<<<<<<<\e[0m"
 systemctl enable rabbitmq-server
-systemctl restart rabbitmq-server
+systemctl start rabbitmq-server
 
 echo -e "\e[36m>>>>>>>>> Add Application User in RabbtiMQ <<<<<<<<\e[0m"
 rabbitmqctl add_user roboshop ${rabbitmq_appuser_password}
