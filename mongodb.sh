86ee2c0512b1a1553132035a8c475a6989ddf28a
manoj <gandhamanojkutty@gmail.com>
2023-11-15 13:12:07 +0530

    code updated

--- mongodb.sh
+++ mongodb.sh
@@ -2,9 +2,15 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-
+echo -e "\e[32m--------copy repo mongo repo file-------\e[0m"
 cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[32m--------install momgodb-------\e[0m"
 dnf install mongodb-org -y
+
+echo -e "\e[32m--------update listen address port-------\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+
+echo -e "\e[32m--------start mongodb service-------\e[0m"
 systemctl enable mongod
 systemctl restart mongod
