e1a2d2f66b5b08a461ccb62428403dee587886c0
manoj <gandhamanojkutty@gmail.com>
2023-11-15 16:03:49 +0530

    code updated

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,24 +1,21 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-rabbitmq_app_password=$1
+rabbitmq_appuser_password=$1
 
-echo -e "\e[32m--------Configure YUM Repos-------\e[0m"
+echo -e "\e[36m>>>>>>>>> Setup ErLang Repos <<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 
-echo -e "\e[32m--------Configure YUM Repos for RabbitMQ-------\e[0m"
+echo -e "\e[36m>>>>>>>>> Setup RabbitMQ Repos <<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
 
+echo -e "\e[36m>>>>>>>>> Install ErLang & RabbitMQ <<<<<<<<\e[0m"
+yum install erlang rabbitmq-server -y
 
-echo -e "\e[32m--------Install RabbitMQ-------\e[0m"
-dnf install rabbitmq-server -y
-
-echo -e "\e[32m--------Start RabbitMQ-------\e[0m"
+echo -e "\e[36m>>>>>>>>> Start RabbitMQ Service <<<<<<<<\e[0m"
 systemctl enable rabbitmq-server
 systemctl restart rabbitmq-server
 
-echo -e "\e[32m--------application user-------\e[0m"
-rabbitmqctl add_user roboshop ${rabbitmq_app_password}
-
-echo -e "\e[32m--------set_permissions for user-------\e[0m"
+echo -e "\e[36m>>>>>>>>> Add Application User in RabbtiMQ <<<<<<<<\e[0m"
+rabbitmqctl add_user roboshop ${rabbitmq_appuser_password}
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
