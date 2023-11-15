b1ffe8d8cadf5ea4c30c6d46274b6a3fbfe744fe
manoj <gandhamanojkutty@gmail.com>
2023-11-15 14:34:49 +0530

    code updated

--- user.sh
+++ user.sh
@@ -2,6 +2,7 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
+echo -e "\e[32m--------disable nodejs-------\e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 
