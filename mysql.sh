578e8bf242244ca6ad1a0732b2bbb22861d75e4e
manoj <gandhamanojkutty@gmail.com>
2023-11-16 13:03:00 +0530

    code updated

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+mysql_password=$1
 
 echo -e "\e[32m--------disable default mysql version-------\e[0m"
 dnf module disable mysql -y
@@ -16,4 +17,4 @@ systemctl enable mysqld
 systemctl restart mysqld
 
 echo -e "\e[32m--------reset mysql password-------\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass ${mysql_password}
