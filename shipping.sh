9faa4894dac1dd369777e73a45a867a9e9c6d053
manoj <gandhamanojkutty@gmail.com>
2023-11-16 12:36:28 +0530

    code updated

--- shipping.sh
+++ shipping.sh
@@ -1,47 +1,38 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-mysql_root_password=$1
 
+echo -e "\e[36m>>>>>>>>> Install Maven <<<<<<<<\e[0m"
+yum install maven -y
 
-echo -e "\e[32m--------install maven-------\e[0m"
-dnf install maven -y
-
-echo -e "\e[32m--------Add application User-------\e[0m"
+echo -e "\e[36m>>>>>>>>> Create App User <<<<<<<<\e[0m"
 useradd ${app_user}
 
-echo -e "\e[32m--------setup app dir-------\e[0m"
+echo -e "\e[36m>>>>>>>>> Create App Directory <<<<<<<<\e[0m"
 rm -rf /app
 mkdir /app
 
-echo -e "\e[32m--------Download the application code-------\e[0m"
+echo -e "\e[36m>>>>>>>>> Download App Content <<<<<<<<\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
 
-echo -e "\e[32m--------unzip content-------\e[0m"
+echo -e "\e[36m>>>>>>>>> Extract App Content <<<<<<<<\e[0m"
+cd /app
 unzip /tmp/shipping.zip
 
-echo -e "\e[32m--------download the dependencies & build the application-------\e[0m"
+echo -e "\e[36m>>>>>>>>> Download Maven Dependencies <<<<<<<<\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
-echo -e "\e[32m--------Setup SystemD Shipping Service-------\e[0m"
+echo -e "\e[36m>>>>>>>>> Install MySQL <<<<<<<<\e[0m"
+yum install mysql -y
+
+echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<\e[0m"
+mysql -h mysql-dev.mdevops333.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
+echo -e "\e[36m>>>>>>>>> Setup SystemD Service <<<<<<<<\e[0m"
 cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
 
-echo -e "\e[32m--------Load the service-------\e[0m"
+echo -e "\e[36m>>>>>>>>> Start Shipping Service <<<<<<<<\e[0m"
 systemctl daemon-reload
-
-echo -e "\e[32m--------install mysql client-------\e[0m"
-dnf install mysql -y
-
-echo -e "\e[32m--------load sheema-------\e[0m"
-mysql -h mysql-dev.mdevops333.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-
-echo -e "\e[32m--------start the shipping service-------\e[0m"
 systemctl enable shipping
 systemctl restart shipping
-
-
-
-
-
