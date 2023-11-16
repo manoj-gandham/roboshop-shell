67ca1c6bb0ff8a94afd2935ac914f904843ab369
manoj <gandhamanojkutty@gmail.com>
2023-11-16 13:27:52 +0530

    code updated

--- common.sh
+++ common.sh
@@ -2,34 +2,38 @@ app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
+print_head() {
+  echo -e echo -e "\e[35m--------$*-------\e[0m"
+}
+
 func_nodejs() {
-  echo -e "\e[32m--------configure nodejs repos-------\e[0m"
+  print_head "configure nodejs repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-  echo -e "\e[32m--------install nodejs-------\e[0m"
+  print_head "install nodejs"
   dnf install nodejs -y
 
-  echo -e "\e[32m--------add application user-------\e[0m"
+  print_head "add application user"
   useradd ${app_user}
 
-  echo -e "\e[32m--------create application dir-------\e[0m"
+  print_head "create application dir"
   rm -rf /app
   mkdir /app
 
-  echo -e "\e[32m--------download app content-------\e[0m"
+  print_head "download app content"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
   cd /app
 
-  echo -e "\e[32m--------unzip app content-------\e[0m"
+  print_head "unzip app content"
   unzip /tmp/${component}.zip
 
-  echo -e "\e[32m--------install npm dependencies-------\e[0m"
+  print_head "install npm dependencies"
   npm install
 
-  echo -e "\e[32m--------copy catalogue systemd service file-------\e[0m"
+  print_head "copy catalogue systemd service file"
   cp $script_path/${component}.service /etc/systemd/system/${component}.service
 
-  echo -e "\e[32m--------start catalogue service-------\e[0m"
+  print_head "start catalogue service"
   systemctl daemon-reload
   systemctl enable ${component}
   systemctl restart ${component}
