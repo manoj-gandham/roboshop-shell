f951ef03831f33de1f5a212c04e52b81e7bd5885
manoj <gandhamanojkutty@gmail.com>
2023-11-17 19:05:16 +0530

    code updated

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,27 @@
 script_path=$(dirname $0)
 source ${script_path}/common.sh
 
-
+func_ststus_check() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[36mSUCESS\e[0m"
+  else
+    echo -e "\e[31mERROR\e[0m"
+  fi
+}
 dnf install nginx -y
+func_ststus_check
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+func_ststus_check
 rm -rf /usr/share/nginx/html/*
+func_ststus_check
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+func_ststus_check
 cd /usr/share/nginx/html
+func_ststus_check
 unzip /tmp/frontend.zip
+func_ststus_check
 systemctl restart nginx
+func_ststus_check
 systemctl enable nginx
-if [ $? -eq 0 ]; then
-  echo -e "\e[36mSUCESS\e[0m"
-else
-  echo -e "\e[31mERROR\e[0m"
-fi
+func_ststus_check
+
