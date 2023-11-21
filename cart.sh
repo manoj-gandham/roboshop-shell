9af92f119356fa3bb1d40b3a7cf2ceb5f506b694
manoj <gandhamanojkutty@gmail.com>
2023-11-21 14:35:10 +0530

    code updated

--- cart.sh
+++ cart.sh
@@ -4,5 +4,10 @@ source ${script_path}/common.sh
 
 component=cart
 
+func_print_head "disable default nodejs version and enable required version"
+dnf module disable nodejs -y &>>$log_file
+dnf module enable nodejs:18 -y &>>$log_file
+func_status_check $?
+
 func_nodejs
 
